4a06afe171118aa5c4d4faf1982c5bd306ac1eb6
Shuvo <shuvombm02@gmail.com>
2020-01-28 15:12:56 +0600

    Buid stop watch app

---
+++ precache-manifest.543be32cbb278eceb156372b5ec9a586.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90317070b0241c566c478578404c855d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3f702307dd9101201c9",
+    "url": "/static/css/main.15a41864.chunk.css"
+  },
+  {
+    "revision": "265ad35d8c2751eb643f",
+    "url": "/static/js/2.ada27a34.chunk.js"
+  },
+  {
+    "revision": "b3f702307dd9101201c9",
+    "url": "/static/js/main.e8335f9b.chunk.js"
+  },
+  {
+    "revision": "a083f22761352fdfcb41",
+    "url": "/static/js/runtime-main.8dd616fa.js"
+  }
+]);
